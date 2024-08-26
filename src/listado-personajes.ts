0d85f8861d88166423778be836ba37cfb43a34e9
Guste Gaubaite <219.guste@gmail.com>
2024-08-26 17:00:13 +0200

    Agregado .join a las habilidades para que estén separadas con su respectiva coma

--- src/listado-personajes.ts
+++ src/listado-personajes.ts
@@ -8,11 +8,13 @@ const crearContenedorPersonaje = (personaje: Personaje): HTMLDivElement => {
   const elementoPersonaje = document.createElement("div");
   elementoPersonaje.classList.add("personaje-contenedor");
   elementoPersonaje.innerHTML = `
-  <img src="http://localhost:3000/${personaje.imagen}" alt="${personaje.nombre}">
+  <img src="http://localhost:3000/${personaje.imagen}" alt="${
+    personaje.nombre
+  }">
   <div class="contenedor-info">
   <p><strong>Nombre</strong>: ${personaje.nombre}</p>
   <p><strong>Especialidad</strong>: ${personaje.especialidad}</p>
-  <p><strong>Habilidades</strong>: ${personaje.habilidades[0]}, ${personaje.habilidades[1]}, ${personaje.habilidades[2]}</p>
+  <p><strong>Habilidades</strong>: ${personaje.habilidades.join(", ")}</p>
   </div>
   `;
 
